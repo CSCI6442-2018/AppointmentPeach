fb4d6bf411d30dad0248cc29e85ebd258179099a
Zhiyuan Ling <vito9509@gmail.com>
2018-03-23 15:43:39 -0400

    create a installable plugin

---
+++ AppointmentPeach.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name:  AppointmentPeach
+Plugin URI:   http://appointmentpeach.com
+Description:  Self-Booked Customer Appointments
+Version:      0.0.1
+Author:       GWU CSCI6442 2018
+Author URI:   https://github.com/CSCI6442-2018
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  AppointmentPeach
+Domain Path:  /languages
+*/
+
